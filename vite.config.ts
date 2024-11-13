f2434ca6ff35445af41f315380e8425463ab4f86
awscodeapp@gmail.com <awscodeapp@gmail.com>
2024-11-13 15:33:27 -0600

    Created files in order to upload page to GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/medicinaparagastritis/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
