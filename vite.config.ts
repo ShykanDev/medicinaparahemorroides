0551c64b5a55528f4516721304301ce9fb7901ec
Shykan <shykadev@gmail.com>
2024-11-22 00:49:26 -0600

    Fixed base

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/medicinaparahemorroides/',
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
